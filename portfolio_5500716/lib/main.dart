8779d630ac7ea841a752e3dab600ea27bce59b74
Stylianos20 <167615811+Stylianos20@users.noreply.github.com>
2024-04-22 09:57:16 +0200

    Projekt

--- portfolio_5500716/lib/main.dart
+++ portfolio_5500716/lib/main.dart
@@ -39,15 +39,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -104,12 +95,37 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
 
+      floatingActionButton: Row(
+children: [
+    FloatingActionButton.large(
+        onPressed: _incrementCounterMin,
+        tooltip: 'Minus',
+        child: const Icon(Icons.remove),
+      ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  FloatingActionButton.large(
+    onPressed: _incrementCounterDiv,
+    tooltip: 'Dividieren',
+    child: const Icon(Icons.call_made_rounded),
+  ),
+
+
+  FloatingActionButton.large(
+    onPressed: _incrementCounterMax,
+    tooltip: 'Multiplizieren',
+    child: const Icon(Icons.dangerous_outlined),
+  ),
+
+
+  FloatingActionButton.large(
+    onPressed: _incrementCounter,
+    tooltip: 'Plus',
+    child: const Icon(Icons.add),
+  ),
+
+    ],// This trailing comma makes auto-formatting nicer for build methods.
+    ),
     );
+
   }
 }
