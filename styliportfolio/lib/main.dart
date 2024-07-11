9e4b1c0e12b6c5a3cabb68fb75fa3bffa76e324d
Stylianos20 <167615811+Stylianos20@users.noreply.github.com>
2024-07-11 13:22:29 +0200

    Projekt / Aufgabe 2 (styliportfolio)

--- styliportfolio/lib/main.dart
+++ styliportfolio/lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'Pages/home_page.dart';
 import 'Pages/lebenslauf_page.dart';
 import 'Pages/faehigkeiten_page.dart';
 import 'Pages/uebermich_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
